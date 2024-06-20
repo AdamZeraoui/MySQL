caa3759724ba6882a114fc89498af98396054815
AdamZeraoui <adam.zeraoui@hotmail.fr>
2024-06-20 16:32:48 +0200

    MAJ

--- connecitionPHP_SQL.php
+++ connecitionPHP_SQL.php
@@ -21,20 +21,35 @@ $recipeStatement = $mysqlClient->prepare('SELECT * FROM recipe');
 $recipeStatement->execute();
 $recipe = $recipeStatement->fetchAll();
 
-foreach($recipe as $allRecipe) {
-    ?><p><?php echo $allRecipe['recipe_name'];?> <?php
-    echo $allRecipe['preparation_time']; ?> min</p>
-    <?php
-}
-    
-    
-$recipeIngredient = $mysqlClient->prepare('SELECT ingredient_name FROM ingredient');
+$recipeIngredient = $mysqlClient->prepare('SELECT ingredient_name,id_ingredient FROM ingredient');
 $recipeIngredient ->execute();
-$ingredient= $recipeIngredient->fetchAll();
+$ingredient= $recipeIngredient->fetchAll(); 
 
-foreach($ingredient as $allIngredient) {
-    ?><p><?php echo $allIngredient['ingredient_name'];?></p>
-    <?php
-}
+$lienRecipe = $mysqlClient->prepare('SELECT id_recipe, id_ingredient FROM recipe_ingedients' );
+$lienRecipe -> execute();
+$liens= $lienRecipe->fetchAll();    
+
+
     
+
+
+foreach($recipe as $allRecipe){
+    echo '<p>' . $allRecipe['recipe_name'] . ' ' . $allRecipe['preparation_time'] . ' min</p>';
+
+    foreach($liens as $allLiens){
+        if($allLiens['id_recipe']== $allRecipe['id_recipe']){
+            foreach($ingredient as $allIngredient){
+                if($allIngredient['id_ingredient'] == $allLiens['id_ingredient']){
+                    echo'<p>' . $allIngredient['ingredient_name'] . '</p>';
+                }
+            }
+        }
+    }
+    echo '<br>';
+}
+
+
+
+//AFFICHER UNE SEUL RECETTE ET LA CATEGORIE POUR LA SUITE
+
 ?>
