b8316904a81d21d3dd6acf40aea4ab8cd6843fab
AdamZeraoui <adam.zeraoui@hotmail.fr>
2024-06-26 15:59:20 +0200

    Update detActor

--- Cinema/controller/CinemaController.php
+++ Cinema/controller/CinemaController.php
@@ -14,23 +14,39 @@ class CinemaController{
     }
     public function listActor(){
         $pdo= Connect::seConnecter();
-        $requete = $pdo->query("SELECT * FROM actor 
-        INNER JOIN person on actor.id_actor = person.id_person"); 
+        $requete = $pdo->query("SELECT * FROM person 
+        INNER JOIN actor ON person.id_person = actor.id_person"); 
 
         require"view/actor/listActor.php";
     }
     /*détails des acteurs*/
     public function detActor($id){
         $pdo= Connect::seConnecter();
-        $requete = $pdo->prepare("SELECT * FROM actor INNER JOIN person on actor.id_actor = person.id_person WHERE id_actor = :id ");
+        $requete = $pdo->prepare("SELECT DISTINCT person.first_name, person.last_name, person.gender, DATE_FORMAT(person.birthday, '%d/%m/%Y') AS fr_birthday
+        FROM play 
+        INNER JOIN actor ON play.id_actor = actor.id_actor
+        LEFT JOIN person ON actor.id_person = person.id_person
+        LEFT JOIN film ON play.id_film = film.id_film
+        LEFT JOIN charactere ON play.id_charactere = charactere.id_charactere
+        WHERE play.id_actor = :id ");
         $requete->execute(["id" => $id]);
+
+        $requeteRole = $pdo->prepare("SELECT DISTINCT film.publication, film.title, charactere.role_name, ROUND(ranking,1) as r_ranking
+        FROM play 
+        INNER JOIN actor ON play.id_actor = actor.id_actor
+        LEFT JOIN person ON actor.id_person = person.id_person
+        LEFT JOIN film ON play.id_film = film.id_film
+        LEFT JOIN charactere ON play.id_charactere = charactere.id_charactere
+        WHERE play.id_actor = :id 
+        ORDER BY publication ASC");
+        $requeteRole->execute(["id" => $id]);
         require"view/actor/detActor.php";
     }
 
     public function listDirector(){
         $pdo= Connect::seConnecter();
-        $requete = $pdo->query("SELECT * FROM director 
-        INNER JOIN person on director.id_director = person.id_person"); 
+        $requete = $pdo->query("SELECT * FROM person
+        INNER JOIN director ON person.id_person = director.id_person"); 
 
         require"view/director/listDirector.php";
     }
--- Cinema/view/actor/detActor.php
+++ Cinema/view/actor/detActor.php
@@ -2,25 +2,35 @@
 
 <table class="uk-table uk-table-striped">
     <thead>
+            
+    <?php
+            $actor=$requete->fetch()?>
+            <p><?= $actor["fr_birthday"]  ?>
+            <p><?= $actor["gender"]  ?>
         <tr>
-            <th>NOM et PRENOM</th>
-            <th>ANNEE DE NAISSANCE</th>
+            <th>PARUTION</th>
+            <th>FILMOGRAPHIE</th>
+            <th>PERSONNAGE</th>
+            <th>NOTE</th>
         </tr>
     </thead>
     <tbody>
+
         <?php
-            foreach($requete->fetchALL() as $actor){?>
-                <tr>
-                    <td><?= $actor["last_name"].' '. $actor["first_name"]; ?></td>
-                    <td><?= $actor["birthday"]  ?></td>
+            foreach($requeteRole->fetchALL() as $role){?><tr>
+                    <td><?= $role["publication"]  ?></td>
+                    <td><?= $role["title"]  ?></td>
+                    <td><?= $role["role_name"]  ?></td>
+                    <td><?= $role["r_ranking"]  ?> / 5</td>
                 </tr>
-    <?php   } ?>
+        <?php   } ?>
+        
     </tbody>
 </table>
 
 <?php
 
-$titre = "Détail de ".$actor["last_name"].' '. $actor["first_name"] ;
-$titre_secondaire = "Détail de ".$actor["last_name"].' '. $actor["first_name"] ;
+$titre = "Détail de ".$actor["first_name"].' '. $actor["last_name"] ;
+$titre_secondaire = "Détail de ".$actor["first_name"].' '. $actor["last_name"] ;
 $contenu = ob_get_clean();
 require "view/template.php";
