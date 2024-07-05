d87b3598e3f17d7f609139b77188873517b3b91a
AdamZeraoui <adam.zeraoui@hotmail.fr>
2024-07-05 16:44:01 +0200

    Update Css

--- Cinema/view/template.php
+++ Cinema/view/template.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="front/Cinema.css">
+    <link rel="stylesheet" href="public/css/Cinema.css">
 <!-- remplire LINK and META -->
     <title><?= $titre ?></title>
 </head>
 <body>
     <div id ="wrapper" class = "uk-container uk-container-expand">
-        <nav>
+        <nav class = "top">
             <ul>
                 <li><a href="index.php?action=listFilms">Films</a></li>
                 <li><a href="index.php?action=listActors">Acteurs</a></li>
